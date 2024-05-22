b53f1a3bf559daed80336eb48240bf99434e419a
rchacons <rchacons97@gmail.com>
2024-05-22 15:31:22 +0200

    Add settings file + CORS handling

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,13 +3,31 @@ from fastapi import FastAPI, UploadFile, File
 #from .dependencies import get_query_token, get_token_header
 #from .internal import admin
 from .routers import comparison
+from .core.config import settings
+from starlette.middleware.cors import CORSMiddleware
 
 #app = FastAPI(dependencies=[Depends(get_query_token)])
-app = FastAPI(title="SI-REL2 API", version="0.1.0")
 
-api_prefix = "/api/v1"
-app.include_router(comparison.router,prefix=api_prefix)
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+    version=settings.API_VERSION,
+)
 
+# Set all CORS enabled origins
+if settings.BACKEND_CORS_ORIGINS:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[
+            str(origin).strip("/") for origin in settings.BACKEND_CORS_ORIGINS
+        ],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+
+app.include_router(comparison.router,prefix=settings.API_V1_STR)
 
 
 @app.get("/", summary="Root endpoint", description="This is the root endpoint of the API.")
