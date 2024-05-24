be1e47ff315770c7eef707af1c4e668940ee5c2b
jperrier <julien.perrier@etudiant.univ-rennes1.fr>
2024-05-24 17:16:05 +0200

    [BACK] update login_for_access_token parameter type

--- backend/app/routers/authentication.py
+++ backend/app/routers/authentication.py
@@ -6,6 +6,8 @@ from ..schemas.token_schema import Token
 from ..auth.auth_handler import authenticate_user, create_access_token
 import logging
 from ..core.config import settings
+from ..schemas.login_schema import LoginSchema  # Importez le schéma de connexion
+
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -24,21 +26,21 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl=settings.API_V1_STR+"/token")
                  200: {"description": "Access token successfully generated and returned."},
                  401: {"description": "Invalid username or password. Authentication failed."},
                  500: {"description": "An error occurred during the authentication process."}})
-def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
-    
+def login_for_access_token(credentials: LoginSchema):
+
     logger.info("Attempting to authenticate user")
 
-    user = authenticate_user(form_data.username, form_data.password)
+    user = authenticate_user(credentials.username, credentials.password)
     if not user:
-        logger.warning("Authentication failed for user: %s", form_data.username)
+        logger.warning("Authentication failed for user: %s", credentials.username)
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    logger.info("User authenticated successfully: %s", form_data.username)
-    
+    logger.info("User authenticated successfully: %s", credentials.username)
+
     token = Token(access_token=create_access_token(user), token_type="bearer")
-    logger.info("Access token created for user: %s", form_data.username)
-    
+    logger.info("Access token created for user: %s", credentials.username)
+
     return token
