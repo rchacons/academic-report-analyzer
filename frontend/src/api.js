14a57ccb60b697b5981c7bf5d1f6f30e2a113e3c
jperrier <julien.perrier@etudiant.univ-rennes1.fr>
2024-05-26 18:16:37 +0200

    [FRONT] regeneration of token when expired

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -8,6 +8,7 @@ const api = axios.create({
   },
 });
 
+// Interceptor de requête pour ajouter le token aux headers
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
@@ -15,5 +16,42 @@ api.interceptors.request.use((config) => {
   }
   return config;
 });
+
+// Interceptor de réponse pour gérer les erreurs 401 ou 403(Unauthorized ou Forbidden)
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+
+    // Si une erreur 401 ou 403 est renvoyée
+    if (
+      error.response &&
+      (error.response.status === 401 || error.response.status === 403) &&
+      !originalRequest._retry
+    ) {
+      originalRequest._retry = true;
+
+      try {
+        console.log('Regénération du token...');
+        await login();
+
+        // Mettre à jour l'en-tête Authorization de la requête originale
+        originalRequest.headers.Authorization = `Bearer ${localStorage.getItem(
+          'token'
+        )}`;
+
+        // Relancer la requête originale avec le nouveau token
+        return api(originalRequest);
+      } catch (err) {
+        console.error('Erreur lors du renouvellement du token:', err);
+        return Promise.reject(error);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 export default api;
