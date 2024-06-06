70d0c10745107961e436c8e795db0240c56ffe54
Manh-Huan <manhhuan.nguyen@orange.com>
2024-06-06 16:13:17 +0200

    feature: extract list of book

--- backend/app/routers/comparison.py
+++ backend/app/routers/comparison.py
@@ -1,8 +1,10 @@
-from ..services.list_book_compare_service import ListBookCompareService
+
+from typing import List
+from ..services.list_book_compare_service import ListBookCompareService, BookListExtractorService
 from fastapi import APIRouter, Depends, HTTPException, UploadFile, File
 from ..services.file_service import save_pdf_temporarily
 from ..services.comparison_service import ComparisonService
-from ..schemas.comparison_schema import ComparaisonListBookResult, ComparisonSubjectsResult
+from ..schemas.comparison_schema import Book, ComparaisonListBookResult, ComparisonSubjectsResult
 from ..auth.auth_bearer import JWTBearer
 import logging
 
@@ -79,6 +81,40 @@ def compare_book_list(file1: UploadFile = File(..., description="The old PDF CAP
         comparison_list_book_service = ListBookCompareService()
         
         return comparison_list_book_service.compareTwoListBook(file1_path, file2_path)
+        
+    except Exception as e:
+
+        logger.exception("An error occurred during comparison")
+        raise HTTPException(status_code=500, detail=str(e))
+    
+
+@router.post("/etract-book-list/",
+             summary="Extract list of book from PDF CAPES list of book",
+             description="This endpoint accepts a PDF CAPES list of book and returns the list of book in this PDF.",
+             response_model=List[Book],
+             responses={
+                 200: {"description": "Extract completed successfully"},
+                 400: {"description": "Invalid input - not a PDF file"},
+                 500: {"description": "An error occurred during extracting list of book"}
+             })
+def extract_book_list(file1: UploadFile = File(..., description="The PDF CAPES list of book to extract")):
+    
+    logger.info("Received request to compare list of book")
+
+
+    # Verify that file is PDFs
+    if file1.content_type != 'application/pdf':
+        logger.error("Invalid file format")
+        raise HTTPException(status_code=400, detail="Files must be in PDF format.")
+
+    # Save the PDF files temporarily    
+    file1_path = save_pdf_temporarily(file1)
+
+    try: 
+        # Compare the contents
+        book_list_extractor_service = BookListExtractorService(file1_path)
+        
+        return book_list_extractor_service.extract_books_from_pdf()
         
     except Exception as e:
 
